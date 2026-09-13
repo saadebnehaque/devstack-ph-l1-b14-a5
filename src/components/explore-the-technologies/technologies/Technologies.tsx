e15e5a3de91134347a6e7a0e7c157e9ff9f877db
Saad <sebnehaque@gmail.com>
2026-09-13 22:52:52 +0600

    fix: update Technologies grid layout for responsive tablet view

--- src/components/explore-the-technologies/technologies/Technologies.tsx
+++ src/components/explore-the-technologies/technologies/Technologies.tsx
@@ -11,7 +11,7 @@ export default function Technologies({ technologiesData, handleAddedToStack, add
 
     return (
 
-        <div className=" lg:col-span-9 grid grid-cols-1 lg:grid-cols-3 gap-3.5 lg:gap-5">
+        <div className=" lg:col-span-9 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3.5 lg:gap-5">
             {
                 technologiesData.map(technology =>
                     <TechnologyCard
